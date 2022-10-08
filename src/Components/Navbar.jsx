63e865b178678e6565c90faf27348387f80d16e1
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-10-09 03:07:08 +0530

    made-navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className="logo">
+            <img id='logo-img' src='./logo192.png' alt='logo-img' />
+            The Movie App
+        </div>
+        
+        <div className="menus">
+            <ul className='menu'>
+                <li className='nav-item'>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/watchlist'>Watchlist</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/contact'>Contact</Link>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
