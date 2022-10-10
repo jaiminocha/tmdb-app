5d1e7a33c6e993512cf179e94adf81002a0c5b86
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-10-10 16:43:53 +0530

    made-footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={ <Home /> }></Route>
         <Route path='/watchlist' element={ <Watchlist /> } />
-        <Route path='/contact' element= {<Contact /> } />
+        <Route path='/contact' element= { <Contact /> } />
       </Routes>
     </Router>
   );
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="inner-footer">
+        <div className="footer-text">
+          Software Developer, situated in Delhi, India. 
+        </div>
+        <div className="contact">
+          <ul className="icons">
+            <h3>Contact Us</h3>
+            <li>Jai Minocha</li>
+            <li>jaiminochaa@gmail.com</li>
+            <li>+91xxxxxxxxxx</li>
+          </ul>
+        </div>
+      </div>
+      <p className="copyright">Copyright &copy; 2022 The Movie App | All rights reserved.</p>
+    </footer>
+  )
+}
